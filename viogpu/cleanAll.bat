104184b37e25b944e33f136b24a753bc80bb0ad4
Vadim Rozenfeld <vrozenfe@redhat.com>
2017-05-30 13:40:16 +1000

    small adjustments toward common with full wddm driver build

--- viogpu/cleanAll.bat
+++ viogpu/cleanAll.bat
@@ -1,6 +1,8 @@
 @echo on
 
 rmdir /S /Q Install
+rmdir /S /Q Debug
+rmdir /S /Q Release
 
 del /F *.log *.wrn *.err
 
@@ -8,3 +10,12 @@ cd viogpudo
 call clean.bat
 cd ..
 
+cd viogpu3d
+call clean.bat
+cd ..
+
+cd display
+cd d3d
+call clean.bat
+cd ..
+cd ..
