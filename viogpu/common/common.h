0ae4e262580789e91928ba3e5eb18af7f3711296
Vadim Rozenfeld <vrozenfe@redhat.com>
2017-06-01 11:24:15 +1000

    update common data

--- viogpu/common/common.h
+++ viogpu/common/common.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define VIOGPU_D3D_MAGIC        0x476F6956 // 'VioG'
+#define VIOGPU_D3D_VERSION      0x1
+
 typedef struct _VIOGPU_PRIVATEDATA {
     ULONG Magic;
     ULONG Version;
